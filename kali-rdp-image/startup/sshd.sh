993cf097857637f563984e60c6ed98ea6a1297cc
Trigus42 <59501676+Trigus42@users.noreply.github.com>
2026-06-01 18:49:01 +0200

    feat: add SSH key-based auth with secret/volume mount
    - Add minimal sshd_config with password auth disabled
    - Add startup script to install authorized_keys for kali user
    - Add Secret and NodePort Service to deployment example
    - Add optional authorized_keys volume mount to docker-compose example

--- kali-rdp-image/startup/sshd.sh
+++ kali-rdp-image/startup/sshd.sh
@@ -2,5 +2,14 @@
 
 if [[ ${SSHD_ENABLE} == 'true' ]]
 then
+	# Set up authorized_keys for kali user from mounted secret
+	if [[ -f /etc/ssh/authorized_keys/authorized_keys ]]; then
+		mkdir -p /home/kali/.ssh
+		cp /etc/ssh/authorized_keys/authorized_keys /home/kali/.ssh/authorized_keys
+		chmod 700 /home/kali/.ssh
+		chmod 600 /home/kali/.ssh/authorized_keys
+		chown -R kali:kali /home/kali/.ssh
+	fi
+
 	/usr/sbin/sshd -D -e
 fi
